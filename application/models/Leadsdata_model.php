73a98b7cfbe4a3056949a5b03463767dc24ead18
Rajesh <rkumargupta798@gmail.com>
2022-01-01 18:24:50 +0530

    1jan2022

--- application/models/Leadsdata_model.php
+++ application/models/Leadsdata_model.php
@@ -63,13 +63,26 @@ class Leadsdata_model extends CRM_Model
 			$query = $this->db->get();
 			return $query->result();
 		}else{
-			$this->db->select('*');
+			/*$this->db->select('*');
 			$this->db->from('tblstaff');
 			$this->db->join('tblleads', 'tblleads.assigned = tblstaff.staffid');
 			$this->db->where(array('tblstaff.pm_assign_to' => $pm_id, 'tblleads.status' => '3' ));
 			// $this->db->order_by('tblleads.lead_booking_amount_date','DESC');
 			$query = $this->db->get();
-			return $query->result();
+			return $query->result();*/
+			
+			$this->db->select('*');
+			$this->db->from('tblstaff');
+			$this->db->join('tblleads', 'tblleads.assigned = tblstaff.staffid');
+			$this->db->where(array('tblstaff.pm_assign_to' => $pm_id, 'tblleads.status' => '3'));
+			$this->db->where("tblleads.lead_booking_amount_date >= tblstaff.pm_assign_date ");
+			//  $this->db->order_by('tblleads.lead_pm_takeup_date','DESC');
+			 $query1 = $this->db->get()->result();
+			//  echo $this->db->last_query();
+			//  die();
+			 $query2 = $this->db->query("SELECT * FROM `tblstaff` JOIN `tblleads` ON `tblleads`.`assigned` = `tblstaff`.`staffid` WHERE `tblstaff`.`pre_pm_assign_to` = '".$pm_id."' AND `tblleads`.`status` = '3' AND tblleads.lead_booking_amount_date < tblstaff.pm_assign_date;")->result();
+			 $query = array_merge($query1,$query2);
+			return $query;
 		}
 		
 
@@ -120,13 +133,26 @@ class Leadsdata_model extends CRM_Model
 			$query = $this->db->get();
 			return $query->result();
 		}else{
-			$this->db->select('*');
+			/*$this->db->select('*');
 			$this->db->from('tblstaff');
 			$this->db->join('tblleads', 'tblleads.assigned = tblstaff.staffid');
 			$this->db->where(array('tblstaff.pm_assign_to' => $pm_id, 'tblleads.pm_project_status' => '2'));
 			$this->db->order_by('tblleads.lead_pm_takeup_date','DESC');
 			$query = $this->db->get();
-			return $query->result();
+			return $query->result();*/
+			
+			$this->db->select('*');
+			$this->db->from('tblstaff');
+			$this->db->join('tblleads', 'tblleads.assigned = tblstaff.staffid');
+			$this->db->where(array('tblstaff.pm_assign_to' => $pm_id, 'tblleads.pm_project_status' => '2'));
+			$this->db->where("tblleads.lead_booking_amount_date >= tblstaff.pm_assign_date ");
+			 $this->db->order_by('tblleads.lead_pm_takeup_date','DESC');
+			 $query1 = $this->db->get()->result();
+			//  echo $this->db->last_query();
+			//  die();
+			 $query2 = $this->db->query("SELECT * FROM `tblstaff` JOIN `tblleads` ON `tblleads`.`assigned` = `tblstaff`.`staffid` WHERE `tblstaff`.`pre_pm_assign_to` = '".$pm_id."' AND `tblleads`.`pm_project_status` = '2' AND tblleads.lead_booking_amount_date < tblstaff.pm_assign_date ORDER BY `tblleads`.`lead_pm_takeup_date` DESC;")->result();
+			 $query = array_merge($query1,$query2);
+			return $query;
 		}
 	}
 
